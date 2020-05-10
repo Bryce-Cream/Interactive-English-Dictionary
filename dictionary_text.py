316d7aa689c02ff726151691cc1f11b2c79f6c00
Bryce Hughson <hughsonb@uwindsor.ca>
2020-05-10 16:20:11 -0400

    Finished The Dictionary User Friendly
    Finished the command line version of this dictionary. Waiting to learn flask to touch up the web app version of it

--- dictionary_text.py
+++ dictionary_text.py
@@ -12,11 +12,24 @@ def findDef(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
-        ##return "Did you mean {get_close_matches(w, data.keys())[0]} instead?"
+        #Change this later to maybe have if the word entered starts with y or n
+        response = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0]).upper()
+        
+        if response == "Y":
+            return data[get_close_matches(w, data.keys())[0]] #User agrees this was the best match
+        elif response == "N":
+            return "The word doesn't exist. Please double check your spelling"
+        else:
+            return "We didn't understand your response"
     else:
         return "The world doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 
-print(f"The definition is:  {findDef(word)}")
+output = findDef(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
