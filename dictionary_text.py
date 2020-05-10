bed933f04082e3c187da9afd1da8b7fc94bf3f92
Bryce Hughson <hughsonb@uwindsor.ca>
2020-05-10 00:14:02 -0400

    Case Sense and Close Matches
    In the process of finishing getting close matches to work just heading to bed

--- dictionary_text.py
+++ dictionary_text.py
@@ -1,15 +1,22 @@
 import json
+from difflib import get_close_matches
 
 #Getting the words and defs from a json file
 #Make it into our own dictionary
 data = json.load(open("data.json"))
 
 #We have it in keys and values so let us make a function to help with that
-def findDef(word):
-    return data[word]
+def findDef(w):
+    w = w.lower() #Case sensitivity
+    #Valid word we have
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        ##return "Did you mean {get_close_matches(w, data.keys())[0]} instead?"
+    else:
+        return "The world doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 
-
 print(f"The definition is:  {findDef(word)}")
-##print(translate(word))
