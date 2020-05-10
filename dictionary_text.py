ea905bd562efb508992b6b629516eed6713f68df
Bryce Hughson <hughsonb@uwindsor.ca>
2020-05-10 17:57:44 -0400

    Fixed certain word erros
    Proper nouns and certain acronyms now function properly such as NATO and Texas

--- dictionary_text.py
+++ dictionary_text.py
@@ -11,6 +11,10 @@ def findDef(w):
     #Valid word we have
     if w in data:
         return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         #Change this later to maybe have if the word entered starts with y or n
         response = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0]).upper()
